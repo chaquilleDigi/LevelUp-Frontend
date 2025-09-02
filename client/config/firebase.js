ada05f9a68866bf24810c6432033a6f3505bdae8
Chaquille  Pratt <prattchaquille@icloud.com>
2025-09-02 11:08:57 -0400

    Create firebase.js

---
+++ client/config/firebase.js
@@ -0,0 +1,15 @@
+// Firebase config (if you plan to use Firebase for notifications or analytics)
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  appId: 'YOUR_APP_ID'
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
