1548bc90b3fc5b041d689aec7f8faf1501f88248
Chaquille  Pratt <prattchaquille@icloud.com>
2025-09-02 10:37:32 -0400

    Create AppNavigator.js

---
+++ client/navigation/AppNavigator.js
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { AuthContext } from '../context/AuthContext';
+import AuthNavigator from './AuthNavigator';
+import HomeScreen from '../screens/HomeScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigator = () => {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {user ? (
+        <>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+        </>
+      ) : (
+        <Stack.Screen name="Auth" component={AuthNavigator} />
+      )}
+    </Stack.Navigator>
+  );
+};
+
+export default AppNavigator;
